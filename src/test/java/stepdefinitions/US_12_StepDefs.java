42dedd61fae5e6cb1544ad762f83d881b4172b05
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-07-05 16:00:16 +0300

    05.07.23

---
+++ src/test/java/stepdefinitions/US_12_StepDefs.java
@@ -0,0 +1,10 @@
+package stepdefinitions;
+
+import pages.LessonMngPage;
+import pages.LoginPage;
+
+public class US_12_StepDefs {
+    LoginPage loginPage = new LoginPage();
+    LessonMngPage lessonMngPage = new LessonMngPage();
+
+}
