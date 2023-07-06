ad837844bdf6e58f684302ac8898c47a5a81b32b
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-07-06 18:42:29 +0300

    stepdefinition started to write

--- src/test/java/stepdefinitions/US_12_StepDefs.java
+++ src/test/java/stepdefinitions/US_12_StepDefs.java
@@ -1,7 +1,40 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.LessonMngPage;
 import pages.LoginPage;
 
 public class US_12_StepDefs {
+
+    @When("user navigate to login page, logged in as a vice dean")
+    public void user_navigate_to_login_page_logged_in_as_a_vice_dean() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user access the lesson program")
+    public void user_access_the_lesson_program() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user select the lesson to assign")
+    public void user_select_the_lesson_to_assign() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user select a teacher from the available teachers")
+    public void user_select_a_teacher_from_the_available_teachers() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click on the {string}")
+    public void user_click_on_the(String string) {
+
+    }
+    @Then("the lesson should be successfully assigned to the selected teacher")
+    public void the_lesson_should_be_successfully_assigned_to_the_selected_teacher() {
+
+    }
+
+
 }
