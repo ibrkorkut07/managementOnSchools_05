ba9cd4dc4f71dc5d3f86112e97491d3b44d3ff90
Yavuz Selim <history.erciyes19@gmail.com>
2023-07-08 18:09:22 +0100

    US_09 Saturday

--- src/test/java/pages/LessonMngPage.java
+++ src/test/java/pages/LessonMngPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class LessonMngPage {
 
     public LessonMngPage() {
@@ -18,7 +20,43 @@ public class LessonMngPage {
     @FindBy(id = "controlled-tab-example-tab-lessonsList")
     public WebElement lessonButton;
 
+    @FindBy(xpath = "(//tbody[@class='table-group-divider'])[2]/tr/td[1]")
+    public List<WebElement> allLessonNames;
+
+    @FindBy(xpath = "(//tbody[@class='table-group-divider'])[2]/tr/td[2]")
+    public List<WebElement> allCompulsories;
+
+    @FindBy(xpath = "(//tbody[@class='table-group-divider'])[2]/tr/td[3]")
+    public List<WebElement> allCreditScoreNames;
 
 
+    @FindBy(id = "lessonName")
+    public WebElement lessonNameButton;
 
+    @FindBy(id = "compulsory")
+    public WebElement compulsoryCheck;
+
+
+    @FindBy(id = "lessonName")
+    public WebElement lessonNameInput;
+    @FindBy(id = "creditScore")
+    public WebElement creditScoreInput;
+
+    @FindBy(xpath = "(//button[text()='Submit' and @type='button'])[2]")
+    public WebElement lessonSubmitButton;
+
+    @FindBy(xpath = "//*[@id=\"controlled-tab-example-tabpane-lessonsList\"]/div[2]/div[2]/div/table/tbody")
+    public WebElement lessonCompulsoryCreditScore;
+    @FindBy(xpath = "(//li/a/span[@class='visually-hidden' and text()='Last'])[2]")
+    public WebElement nextButton;
+
+    @FindBy(xpath = "(//button[@class='btn btn-danger'])[last()]")
+    public WebElement deleteLastCreatedLesson;
+
+    @FindBy(xpath = "((//tbody[@class='table-group-divider'])[2]/tr/td[1])[last()]")
+    public WebElement lastCreatedLesson;
 }
+
+
+
+
