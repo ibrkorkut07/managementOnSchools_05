aefc96d977a1bb26cc50f5c6bfcf2961de85d173
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-07-10 11:40:46 +0300

    10.07.2023

--- src/test/java/stepdefinitions/US_12_StepDefs.java
+++ src/test/java/stepdefinitions/US_12_StepDefs.java
@@ -40,4 +40,46 @@ public class US_12_StepDefs {
         Driver.closeDriver();
     }
 
+    @Given("vice dean do not select a teacher from available teachers")
+    public void vice_dean_do_not_select_a_teacher_from_available_teachers() {
+
+    }
+    @Then("vice dean verify the error message should be displayed")
+    public void vice_dean_verify_the_error_message_should_be_displayed() {
+
+    }
+
+    @Given("vice dean select a teacher is already assigned to same lesson at the same time")
+    public void vice_dean_select_a_teacher_is_already_assigned_to_same_lesson_at_the_same_time() {
+
+        }
+
+   @Then("vice dean verify the error message should be displayed indicating {string}")
+   public void vice_dean_verify_the_error_message_should_be_displayed_indicating(String string) {
+
+        }
+
+    @Given("vice dean select multiple lesson in different times to assign")
+    public void vice_dean_select_multiple_lesson_in_different_times_to_assign() {
+
+
+    }
+    @Then("vice dean verify the lessons should be successfully assigned to the selected teacher")
+    public void vice_dean_verify_the_lessons_should_be_successfully_assigned_to_the_selected_teacher() {
+
+
+    }
+    @Given("vice dean select multiple lesson in conflict times to assign")
+    public void vice_dean_select_multiple_lesson_in_conflict_times_to_assign() {
+
+    }
+   // @Then("vice dean verify the error message should be displayed")
+   // public void vice_dean_verify_the_error_message_should_be_displayed () {
+
+
+   // }
+
+
+
+
 }
