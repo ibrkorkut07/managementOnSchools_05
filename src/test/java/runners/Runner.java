1d135ae1194c16db55ceec2c4d5924777433e039
yehatipoglu <127590403+yehatipoglu@users.noreply.github.com>
2023-07-11 16:00:05 +0300

    Yunus's commit

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,12 +18,12 @@ import org.junit.runner.RunWith;
 
         features = "./src/test/resources/features",//PATH OF FEATURES FOLDER
         glue = {"stepdefinitions","hooks"}, //PATH OF STEPDEFINITIONS FOLDER
-        dryRun = true , //dryRun = true : RUNS AND GENERATES ONLY MISSING STEP DEFS.
+        dryRun = false , //dryRun = true : RUNS AND GENERATES ONLY MISSING STEP DEFS.
         // dryRun = false: RUN NORMAL. THIS IS DEFAULT VALUE
 
 
 
-        tags = "@US02_TC01"
+        tags = "@US_05"
 
 
 
--- src/test/java/stepdefinitions/US_05_AdminsSeeDeleteUpdateDeans.java
+++ src/test/java/stepdefinitions/US_05_AdminsSeeDeleteUpdateDeans.java
@@ -1,14 +1,25 @@
 package stepdefinitions;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import pages.AdminMngPage;
+import pages.DeanMngPage;
 import pages.HomePage;
 import pages.LoginPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
+
+import static utilities.Driver.driver;
 
 
 public class US_05_AdminsSeeDeleteUpdateDeans {
@@ -18,44 +29,109 @@ public class US_05_AdminsSeeDeleteUpdateDeans {
 
 
 
+    DeanMngPage deanMngPage=new DeanMngPage();
+    public CharSequence adminPasssword;
+
+    @Then("Admin verify to see Name of Deans")
+    public void admin_verify_to_see_name_of_deans() {
+
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+
+        Assert.assertTrue(deanMngPage.firstNameonDeanList.isDisplayed());
+
+        }
 
 
+    @Then("Admin verify to see Gender of Deans")
+    public void admin_verify_to_see_gender_of_deans() {
+
+    Assert.assertTrue(deanMngPage.firstGenderOnDeanList.isDisplayed());
+
+    }
+    @Then("Admin verify to see Phone Number of Deans")
+    public void admin_verify_to_see_phone_number_of_deans() {
+
+    Assert.assertTrue(deanMngPage.firstPhoneNumberOnDeanList.isDisplayed());
+    }
+    @Then("Admin verify to see SSN of Deans")
+    public void admin_verify_to_see_ssn_of_deans() {
+    Assert.assertTrue(deanMngPage.firstSsnOnDeanList.isDisplayed());
 
 
-    @Then("Admin verify to see information of Dean's")
-    public void adminVerifyToSeeInformationOfDeanS() {
+    }
+    @Then("Admin verify to see User Name of Deans")
+    public void admin_verify_to_see_user_name_of_deans() {
 
-        Asser assert tru is dis
+    Assert.assertTrue(deanMngPage.firstUserNameOnDeanList.isDisplayed());
 
     }
 
+
+
     @Then("Click on {string} button")
     public void clickOnButton(String arg0) {
-        
+
+
     }
 
     @Then("Admin verify that Dean was deleted")
     public void adminVerifyThatDeanWasDeleted() {
+
+
         
     }
 
     @Then("click the {string} button")
-    public void clickTheButton(String arg0) {
-        
+    public void clickTheButton(String arg0) throws InterruptedException {
+        ReusableMethods.waitFor(3);
+
+
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+        ReusableMethods.scrollDownActions();
+
+
+        deanMngPage.editButton.click();
+
+
+
     }
 
     @Then("Enters different data into name field in the edit window")
     public void entersDifferentDataIntoNameFieldInTheEditWindow() {
+
+        deanMngPage.firstNameToUpdate.click();
+        deanMngPage.firstNameToUpdate.sendKeys("xxxxx");
+
+
+
+
+
+
         
     }
 
     @Then("click the submit button in the edit window")
     public void clickTheSubmitButtonInTheEditWindow() {
-        
+
+        deanMngPage.maleButtonOnEditDean.click();
+Driver.wait(2);
+        deanMngPage.passwordBox.sendKeys("12345678");
+
+        deanMngPage.submitButtonOnEditDean.click();
+
+        ReusableMethods.waitFor(3);
+
     }
 
     @Then("close the edit window")
     public void closeTheEditWindow() {
+
         
     }
 
