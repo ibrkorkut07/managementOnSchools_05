c07c9d95c32c7b54b92e6f2b3c8e27b1ac0c0032
nurullahkaleli <nurullahkaleli@hotmail.com>
2023-07-11 12:59:39 +0300

    Committing changes before branch switch

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "pretty",
+               // "pretty",
                 "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
