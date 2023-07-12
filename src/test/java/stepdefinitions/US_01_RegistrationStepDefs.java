9a5d66c979bb05afd4cb08c9100f857c41081694
ibrkorkut07 <ibr.kor@gmail.com>
2023-07-12 17:50:27 +0200

    US01_DB

--- src/test/java/stepdefinitions/US_01_RegistrationStepDefs.java
+++ src/test/java/stepdefinitions/US_01_RegistrationStepDefs.java
@@ -6,24 +6,21 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
 import pages.HomePage;
-import pages.LoginPage;
 import pages.RegisterPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 import utilities.WaitUtils;
 
-import javax.swing.*;
-
-import static utilities.Driver.driver;
+import java.sql.*;
 
 public class US_01_RegistrationStepDefs {
 
-    HomePage homePage = new HomePage();
     RegisterPage regPage = new RegisterPage();
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
 
     /*
     @When("user clicks on Register link")
@@ -354,4 +351,40 @@ public class US_01_RegistrationStepDefs {
     public void userClicksOnRegisterButton() {
         regPage.registerBox.click();
     }
+
+    @Given("Get registered user via {string}")
+    public void getRegisteredUserVia(String username) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:postgresql://managementonschools.com:5432/school_management", "select_user", "43w5ijfso");
+        statement = connection.createStatement();
+        String sqlQuery = "select * from guest_user where name = '"+username+"'";
+        resultSet = statement.executeQuery(sqlQuery);
+        // resultSet.next();
+    }
+
+    @Then("Body contains birth_day {string}, birth_place {string}, gender {string}, name {string}, phone_number {string}, ssn {string}, surname {string}, username {string}")
+    public void bodyContainsBirth_dayBirth_placeGenderNamePhone_numberSsnSurnameUsername(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6, String arg7) throws SQLException {
+        resultSet.next();
+        System.out.println(resultSet.getString("phone_number"));
+    }
+
+
+   /*
+    @Given("Get registered user via {string}")
+    public void getRegisteredUserVia(String username) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:postgresql://managementonschools.com:5432/school_management", "select_user", "43w5ijfso");
+        statement = connection.createStatement();
+        String sqlQuery = "select * from guest_user where name = '"+username+"'";
+        resultSet = statement.executeQuery(sqlQuery);
+        // resultSet.next();
+    }
+
+    @Then("Body contains birth_day {string}, birth_place {string}, gender {string}, name {string}, phone_number {string}, ssn {string}, surname {string}, username {string}")
+    public void bodyContainsBirth_dayBirth_placeGenderNamePhone_numberSsnSurnameUsername(String birth_day, String birth_place, String gender, String name, String phone_number, String ssn, String surname, String username) throws SQLException {
+
+        resultSet.next();
+        System.out.println(resultSet.getString("phone_number"));
+    }
+
+    */
+
 }
