2518df2c0dc364b98006a3053df4ba1395ba1f2a
Meyra Baz <123989144+meyrabaz@users.noreply.github.com>
2023-07-12 12:57:05 +0300

    us 13 stepdefs added

--- src/test/java/stepdefinitions/US_13_ViceDeanCreateTeacherStepDefs.java
+++ src/test/java/stepdefinitions/US_13_ViceDeanCreateTeacherStepDefs.java
@@ -1,5 +1,57 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.ViceDeanMngPage;
+
 public class US_13_ViceDeanCreateTeacherStepDefs {
+    ViceDeanMngPage viceDeanMngPage =  new ViceDeanMngPage();
+
+
+    @Given("vice dean clicks on Teacher Management Option")
+    public void vice_dean_clicks_on_teacher_management_option() {
+
+
+
+    }
+
+    @When("vice dean enter {string}")
+    public void vice_dean_enter(String string) {
+
+
+
+    }
+
+    @When("vice dean enter {string} , {string} , {string}")
+    public void vice_dean_enter(String string, String string2, String string3) {
+
+
+
+    }
+
+    @When("vice dean choose {string}")
+    public void vice_dean_choose(String string) {
+
+
+
+    }
+
+    @When("vice dean enter {string} and {string}")
+    public void vice_dean_enter_and(String string, String string2) {
+
+
+
+    }
+
+    @Then("verify teacher saved successfully")
+    public void verify_teacher_saved_successfully() {
+
+
+
+    }
+
+
+
 
 }
