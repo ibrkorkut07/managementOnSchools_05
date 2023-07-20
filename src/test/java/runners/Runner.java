564c005db432d823e8a6b9c1598171deb1eb9916
AnushaMajeedi <anusha.safi1@gmail.com>
2023-07-21 00:29:31 +0200

    US_16
    New

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
 
         dryRun = false, //dryRun = true : RUNS AND GENERATES ONLY MISSING STEP DEFS.
         // dryRun = false: RUN NORMAL. THIS IS DEFAULT VALUE
-        tags = ""
+        tags = "@my_test16"
 
 )
 public class Runner {
