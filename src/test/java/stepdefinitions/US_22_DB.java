71fe4a45b27e364661140913fda8f79196a91250
yehatipoglu <127590403+yehatipoglu@users.noreply.github.com>
2023-07-26 21:23:45 +0300

    US_22 all done

--- src/test/java/stepdefinitions/US_22_DB.java
+++ src/test/java/stepdefinitions/US_22_DB.java
@@ -8,26 +8,58 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.*;
 import utilities.ConfigReader;
+import utilities.DBUtils;
 import utilities.Driver;
 import utilities.WaitUtils;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class US_22_DB {
 
-@Given("get admin via username")
-public void get_admin_via_username() {
+        ResultSet resultSet;
+
+
+
+        @Given("get admin via username")
+        public void get_admin_via_username() throws SQLException {
+
+
+                String sqlQuery = "SELECT * FROM admins WHERE username = 'YunusAhmet'";
+                resultSet = DBUtils.getResultSet(sqlQuery);
 
 
 
         }
+        @Then("id {string}, username {string}, birth_day {string} , birth_place {string} , gender {string} , name {string}, phone_number {string}, ssn {string}, surname {string}")
+        public void id_username_birth_day_birth_place_gender_name_phone_number_ssn_surname(String id, String username, String birth_day, String birth_place, String gender, String name, String phone_number, String ssn, String surname) throws SQLException {
 
+                resultSet.next();
 
-@Then("id {string}, username {string}, birth_day {string} , birth_place {string} , gender {string} , name {string}, phone_number {string}, ssn {string}, surname{string}")
-public void id_username_birth_day_birth_place_gender_name_phone_number_ssn_surname(String string, String string2, String string3, String string4, String string5, String string6, String string7, String string8, String string9) {
+                String actId=resultSet.getString("id");
+                System.out.println("actId = " + actId);
+                String actBirth_day = resultSet.getString("birth_day");
+                String actBirth_place = resultSet.getString("birth_place");
+                String actGender = resultSet.getString("gender");
+                String actName = resultSet.getString("name");
+                String actPhone_number = resultSet.getString("phone_number");
+                String actSsn = resultSet.getString("ssn");
+                String actSurname = resultSet.getString("surname");
+                String actUsername = resultSet.getString("username");
 
+          //      assertEquals(id,actId);
+                assertEquals(birth_day,actBirth_day);
+                assertEquals(birth_place,actBirth_place);
+                assertEquals(gender,actGender);
+                assertEquals(name,actName);
+                assertEquals(phone_number,actPhone_number);
+                assertEquals(ssn,actSsn);
+                assertEquals(surname,actSurname);
+                assertEquals(username,actUsername);
 
 
 
--- src/test/resources/features/US_22_Add_Admin/US_22_DB.feature
+++ src/test/resources/features/US_22_Add_Admin/US_22_DB.feature
@@ -1,6 +1,10 @@
 @US_22_DB
 Feature: US_22 add admin
 
-  Scenario: Us_22 add admin
+  Scenario Outline: Us_22 add admin DB
     Given get admin via username
-    Then id "<id>", username "<username>", birth_day "<birth_day>" , birth_place "<birth_place>" , gender "<gender>" , name "<name>", phone_number "<phone_number>", ssn "<ssn>", surname"<surname>"
+    Then id "<id>", username "<username>", birth_day "<birth_day>" , birth_place "<birth_place>" , gender "<gender>" , name "<name>", phone_number "<phone_number>", ssn "<ssn>", surname "<surname>"
+
+    Examples:
+      | id    | username   | birth_day  | birth_place | gender | name | phone_number | ssn           | surname |
+      | 457   | YunusAhmet |  1993-12-12|  Amasya     |  0  |Ahmet | 534-643-5431 |  321-53-4673  |  Sahin  |
